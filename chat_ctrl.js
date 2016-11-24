71a433570eb2e6f609c47bc491e56bde1587c7af
“kasper341” <kasper_341@hotmail.com>
2016-11-24 14:31:20 -0800

    add 'ssl' flag

--- chat_ctrl.js
+++ chat_ctrl.js
@@ -10,6 +10,7 @@ app.controller("ChatCtrl", function ($scope, Pubnub) {
     Pubnub.init({
         publish_key:"pub-c-ea1c66ab-94c3-4987-926a-8dd6d3bf504e",
         subscribe_key:"sub-c-8dc59e18-b283-11e6-a071-02ee2ddab7fe",
+        ssl: true,
         uuid: $scope.uuid
     });
 
